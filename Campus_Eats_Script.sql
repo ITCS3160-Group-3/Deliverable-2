5b1e996f9fd27102f353ebe0d4fa1b4784a8207f
aroseber@uncc.edu <64046207+Covertly292@users.noreply.github.com>
2021-05-03 21:02:21 -0400

    Update Campus_Eats_Script.sql

--- Campus_Eats_Script.sql
+++ Campus_Eats_Script.sql
@@ -1,3 +1,11 @@
-SELECT DISTINCT ratings.ratingid, overallrating, order_id, quality, orderagain
-FROM ratings, restaurant_rating
-LIMIT 20;
+SELECT ratings.ratingid, ratings.overallrating, ratings.order_id, restaurant_rating.quality, restaurant_rating.orderagain
+FROM ratings
+INNER JOIN restaurant_rating
+ON ratings.ratingid = restaurant_rating.ratingid
+ORDER BY restaurant_rating.quality DESC
+
+SELECT ratings.ratingid, ratings.overallrating, ratings.order_id, driver_rating.ontime, driver_rating.courteous
+FROM ratings
+INNER JOIN driver_rating
+ON ratings.ratingid = driver_rating.ratingid
+ORDER BY ratings.overallrating ASC
